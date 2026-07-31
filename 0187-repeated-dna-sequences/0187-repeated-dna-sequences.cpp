87faad5e4ec9e80a2ef53fd8d19e57ed76b65360
Niha Hazarika <nihahazarika2005@gmail.com>
2026-07-31 22:15:56 +0530

    Time: 41 ms (60.81%), Space: 22.6 MB (71.7%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_set<string> seen;
+        unordered_set<string> repeated;
+
+        for(int i = 0; i <= s.size(); i++){
+            string temp = s.substr(i, 10);
+
+            if(seen.find(temp) != seen.end()){
+                repeated.insert(temp);
+
+            } else {
+                seen.insert(temp);
+            }
+
+
+
+        }
+
+        return vector<string>(repeated.begin(), repeated.end());
+
+      
+    }
+};
