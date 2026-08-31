a26fdec73e7a19439278cf844acba86e247efb41
Niha Hazarika <nihahazarika2005@gmail.com>
2026-08-31 19:12:21 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87.37%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if(x < 2) {
+            return x;
+        }
+
+        int l = 1;
+        int r = x / 2;
+        int ans = 0;
+
+        while(l <= r){
+            int m = l + (r - l) / 2;
+
+            if(m == x / m){
+                return m;
+
+            } else if(m < x / m){
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans;
+        
+    }
+};
